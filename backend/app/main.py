4a7cd209da94ad35c7dc1a4a93ac72e2a096cb93
dineshrajasujithra04 <dineshrajasujithra04@gmail.com>
2026-08-08 11:13:23 +0530

    Allow deployed frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,6 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174",
+        "https://personalcalendar-e166.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
