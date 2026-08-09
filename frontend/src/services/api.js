a798a2be6216e6d8b37bb94b95a0f8e60179461d
dineshrajasujithra04 <dineshrajasujithra04@gmail.com>
2026-08-09 19:43:37 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,19 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://personal-calendar-backend-rjzx.onrender.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
 });
 
 export default api;
